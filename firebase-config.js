37c374447273b3303b88958f2704a1020ea9b443
khantaib110-cellcc <khantaib110@gmail.com>
2026-09-08 20:09:08 +0530

    Add Firebase configuration and payment key setting

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+// Firebase configuration for Minakshi Aronai Shop.
+const firebaseConfig={apiKey:"AIzaSyD6jo_W5LU_uK0yOdvYFpxuX5eHmWHfAc",authDomain:"minakshi-aronai-shop.firebaseapp.com",projectId:"minakshi-aronai-shop",storageBucket:"minakshi-aronai-shop.firebasestorage.app",messagingSenderId:"247111526668",appId:"1:247111526668:web:71ec40a9e21f28d9960129",measurementId:"G-5E1DW9RBRW"};
+// Public Razorpay Key ID only. Never put the Razorpay Secret Key here.
+// Add your test/live Key ID before enabling online payments.
+const RAZORPAY_KEY_ID="";
+export {firebaseConfig,RAZORPAY_KEY_ID};
